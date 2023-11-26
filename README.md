cc4aa56e62e6e00c9140b5a30ab76e49fc8ac206
djserzhe <93681490+djserzhe@users.noreply.github.com>
2023-11-26 23:53:08 +0400

    tran input decode

--- README.md
+++ README.md
@@ -4,3 +4,10 @@
 
 - BNB_TRANS_URL (URL вида https://go.getblock.io/<токен>/)
 - BNB_TRANS_REQUEST_INTERVAL (интервал в секундах, через который получать транзакции)
+
+# Зависимости
+
+```
+pip3 install python-dotenv 
+pip install web3
+```
--- bnb_trans.py
+++ bnb_trans.py
@@ -2,6 +2,8 @@ import requests
 import json
 import time
 import os
+from web3 import Web3
+from dotenv import load_dotenv
 
 def current_block_number(url: str) -> int:
     try:
@@ -15,13 +17,13 @@ def current_block_number(url: str) -> int:
 
         res = requests.post(url, headers=headers, json=body)
         data = json.loads(res.text)
-        current_block = int(data['result'],16)
+        current_block = int(data['result'], 16)
         return current_block
     except Exception as e:
         print(f'Error while getting last block number: {e}')
         return -1
     
-def transactions_of_block(url: str, block: int):
+def transactions_of_block(url: str, block: int, web3_obj: Web3, abi: json):
     try:
         headers = {'Content-Type': 'application/json'}
         body = {
@@ -34,16 +36,28 @@ def transactions_of_block(url: str, block: int):
         res = requests.post(url, headers=headers, json=body)
         data = json.loads(res.text)
 
-        for tr in data['result']['transactions']:           
-            if int(tr['value'], 16) == 0:
-                continue
-            print(f"{tr['hash']} {tr['from']} {tr['to']} {int(tr['value'], 16) / 10**18}")
+        for tr in data['result']['transactions']:
+            decoded_input = ''
+            if int(tr['value'], 16) == 0 and tr['input'] != '0x':
+                try:
+                    contract = web3_obj.eth.contract(address=Web3.to_checksum_address(tr['to']), abi=abi)
+                    func_obj, func_params = contract.decode_function_input(tr['input'])
+                    decoded_input = f'{func_obj} {func_params}'
+                except Exception as e:
+                    decoded_input = e
+
+            print(f'{tr["hash"]} {tr["from"]} {tr["to"]} {int(tr["value"], 16) / 10**18} {decoded_input}')
     except Exception as e:
         print(f'Error while getting transactions of block {block}: {e}')
 
 def main():
+    dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
+    if os.path.exists(dotenv_path):
+        load_dotenv(dotenv_path)
     url = os.environ['BNB_TRANS_URL']
     request_interval = int(os.environ['BNB_TRANS_REQUEST_INTERVAL'])
+    web3 = Web3(Web3.HTTPProvider('https://bsc-dataseed.binance.org/'))
+    abi = json.loads('[{"inputs":[],"payable":false,"stateMutability":"nonpayable","type":"constructor"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"owner","type":"address"},{"indexed":true,"internalType":"address","name":"spender","type":"address"},{"indexed":false,"internalType":"uint256","name":"value","type":"uint256"}],"name":"Approval","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"previousOwner","type":"address"},{"indexed":true,"internalType":"address","name":"newOwner","type":"address"}],"name":"OwnershipTransferred","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"from","type":"address"},{"indexed":true,"internalType":"address","name":"to","type":"address"},{"indexed":false,"internalType":"uint256","name":"value","type":"uint256"}],"name":"Transfer","type":"event"},{"constant":true,"inputs":[],"name":"_decimals","outputs":[{"internalType":"uint8","name":"","type":"uint8"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"_name","outputs":[{"internalType":"string","name":"","type":"string"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"_symbol","outputs":[{"internalType":"string","name":"","type":"string"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"internalType":"address","name":"owner","type":"address"},{"internalType":"address","name":"spender","type":"address"}],"name":"allowance","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"internalType":"address","name":"spender","type":"address"},{"internalType":"uint256","name":"amount","type":"uint256"}],"name":"approve","outputs":[{"internalType":"bool","name":"","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[{"internalType":"address","name":"account","type":"address"}],"name":"balanceOf","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"internalType":"uint256","name":"amount","type":"uint256"}],"name":"burn","outputs":[{"internalType":"bool","name":"","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"decimals","outputs":[{"internalType":"uint8","name":"","type":"uint8"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"internalType":"address","name":"spender","type":"address"},{"internalType":"uint256","name":"subtractedValue","type":"uint256"}],"name":"decreaseAllowance","outputs":[{"internalType":"bool","name":"","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"getOwner","outputs":[{"internalType":"address","name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"internalType":"address","name":"spender","type":"address"},{"internalType":"uint256","name":"addedValue","type":"uint256"}],"name":"increaseAllowance","outputs":[{"internalType":"bool","name":"","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"internalType":"uint256","name":"amount","type":"uint256"}],"name":"mint","outputs":[{"internalType":"bool","name":"","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"name","outputs":[{"internalType":"string","name":"","type":"string"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"owner","outputs":[{"internalType":"address","name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[],"name":"renounceOwnership","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"symbol","outputs":[{"internalType":"string","name":"","type":"string"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"totalSupply","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"internalType":"address","name":"recipient","type":"address"},{"internalType":"uint256","name":"amount","type":"uint256"}],"name":"transfer","outputs":[{"internalType":"bool","name":"","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"internalType":"address","name":"sender","type":"address"},{"internalType":"address","name":"recipient","type":"address"},{"internalType":"uint256","name":"amount","type":"uint256"}],"name":"transferFrom","outputs":[{"internalType":"bool","name":"","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"internalType":"address","name":"newOwner","type":"address"}],"name":"transferOwnership","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"}]')
 
     current_block = current_block_number(url)
     if current_block == -1:
@@ -52,7 +66,7 @@ def main():
 
     while True:
         for block in range(previous_block, current_block + 1):
-            transactions_of_block(url, block)
+            transactions_of_block(url, block, web3, abi)
         time.sleep(request_interval)
         previous_block = current_block + 1
         current_block = current_block_number(url)
